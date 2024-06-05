499fc9050ebbc53373e82d940bf7f3204d94eedc
colinio43 <colingroenen@gmail.com>
2024-06-05 14:33:17 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,338 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Fietsenwinkel_allerlaatste_poging
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        decimal totaalBetalen = 0;
+
+        private DispatcherTimer timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            pbTimer.Value = 100;
+
+            // Start de timer bij het opstarten
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TimerReset();
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            TimerReset();
+        }
+
+        private void TimerReset()
+        {
+            // Reset the timer and Progress bar
+            timer.Stop();
+            timer.Start();
+            pbTimer.Value = 100;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (pbTimer.Value > 0)
+            {
+                pbTimer.Value--;
+            }
+            else
+            {
+                // Laat zien als de timer vol zit dat het programma gesloten word
+                MessageBox.Show("Je hebt niks gedaan telang");
+                Close();
+            }
+        }
+
+        private void btnRekenmachine_Click(object sender, RoutedEventArgs e)
+        {
+            //zorgt er voor dat er een nieuwe window wordt geopend
+            Rekenmachine mainwWindow = new Rekenmachine();
+            mainwWindow.Show();
+        }
+
+        private void btnBestellen_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                /*zorgt er voor dat je alleen getallen in mag vullen en niet min getallen in mag vullen
+                 alleen als je getallen boven de 1 in vult wordt de code hier onder geactiveerd*/
+                int dagen = Convert.ToInt32(txbAantalDagen.Text);
+                if (dagen >= 1)
+                {
+                    string selectedcmbSoort = cmbFiets.Text;
+                    string selectedcmbVerzekering = cmbVerzekering.Text;
+                    string selectedcmbService = cmbService.Text;
+
+                    if (cmbFiets.SelectedIndex >= 0)
+                    {
+                        ComboBoxItem fiets = (ComboBoxItem)cmbFiets.SelectedItem;
+                        lstVerzameling.Items.Add(fiets.Content.ToString() + " Aantal dagen: " + txbAantalDagen.Text);
+                    }
+
+                    if (cmbVerzekering.SelectedIndex >= 0)
+                    {
+                        ComboBoxItem verzekering = (ComboBoxItem)cmbVerzekering.SelectedItem;
+                        lstVerzameling.Items.Add(verzekering.Content.ToString() + " Aantal dagen: " + txbAantalDagen.Text);
+                    }
+
+                    if (cmbService.SelectedIndex >= 0)
+                    {
+                        ComboBoxItem service = (ComboBoxItem)cmbService.SelectedItem;
+                        lstVerzameling.Items.Add(service.Content.ToString() + " Aantal dagen: " + txbAantalDagen.Text);
+                    }
+
+                    //FIETS
+                    if (txbAantalDagen.Text != null)
+                    {
+                        if (cmbFiets.SelectedIndex != -1)
+                        {
+                            int index = selectedcmbSoort.IndexOf("€") + 1;
+                            int length = selectedcmbSoort.IndexOf(" /") - index;
+                            //hiermee kunnen we dadelijk de prijs ophalen\
+                            //wat hier gebeurt is eigenlijk; hij kijkt hoeveel characters er voor en na de prijs zitten
+
+                            if (index >= 0)
+                            //als die index iets heeft opgehaald dan kan deze dingen gaan uitvoeren
+                            {
+                                string prijsfiets = selectedcmbSoort.Substring(index, length);
+                                //haalt alles weg uit de string van de comboboxitem behalve de prijs van het item
+
+                                string totaalbalans = lbTotaalBedrag.Content.ToString();
+                                totaalBetalen = decimal.Parse(totaalbalans);
+                                decimal prijsFietsdecimal = decimal.Parse(prijsfiets);
+
+
+                                if (txbAantalDagen != null)
+                                {
+                                    if (lbTotaalBedrag.Content == "0")
+                                    {
+                                        lbTotaalBedrag.Content = "1";
+                                    }
+                                    prijsFietsdecimal *= Convert.ToInt32(txbAantalDagen.Text);
+                                    //de prijs maal het aantal geselcteerde dagen
+
+                                }
+
+                                totaalBetalen += prijsFietsdecimal;
+                                lbTotaalBedrag.Content = totaalBetalen.ToString();
+                            }
+                        }
+
+
+                    }
+
+                    //VERZEKERING
+                    if (txbAantalDagen.Text != null)
+                    {
+                        if (cmbVerzekering.SelectedIndex != -1)
+                        {
+                            int index = selectedcmbVerzekering.IndexOf("€") + 1;
+                            int length = selectedcmbVerzekering.IndexOf(" /") - index;
+                            if (index >= 0)
+                            //als die index iets heeft opgehaald dan kan deze dingen gaan uitvoeren
+                            {
+                                string prijsverzekering = selectedcmbVerzekering.Substring(index, length);
+                                //haalt alles weg uit de string van de comboboxitem behalve de prijs van het item
+
+                                string totaalbalans = lbTotaalBedrag.Content.ToString();
+                                totaalBetalen = decimal.Parse(totaalbalans);
+                                decimal prijsVerzekringdecimal = decimal.Parse(prijsverzekering);
+
+
+                                if (txbAantalDagen != null)
+                                {
+                                    if (lbTotaalBedrag.Content == "0")
+                                    {
+                                        lbTotaalBedrag.Content = "1";
+                                    }
+                                    prijsVerzekringdecimal *= Convert.ToInt32(txbAantalDagen.Text);
+                                    //de prijs maal het aantal geselcteerde dagen
+                                }
+
+                                totaalBetalen += prijsVerzekringdecimal;
+                                lbTotaalBedrag.Content = totaalBetalen.ToString();
+                            }
+                        }
+
+                    }
+
+                    //SERVICE
+                    if (txbAantalDagen.Text != null)
+                    {
+                        if (cmbService.SelectedIndex != -1)
+                        {
+                            int index = selectedcmbService.IndexOf("€") + 1;
+                            int length = selectedcmbService.IndexOf(" /") - index;
+                            //hiermee kunnen we dadelijk de prijs ophalen\
+                            //wat hier gebeurt is eigenlijk; hij kijkt hoeveel characters er voor en na de prijs zitten
+
+                            if (index >= 0)
+                            //als die index iets heeft opgehaald dan kan deze dingen gaan uitvoeren
+                            {
+                                string prijsService = selectedcmbService.Substring(index, length);
+                                //haalt alles weg uit de string van de comboboxitem behalve de prijs van het item
+
+                                string totaalbalans = lbTotaalBedrag.Content.ToString();
+                                totaalBetalen = decimal.Parse(totaalbalans);
+                                decimal prijsServicedecimal = decimal.Parse(prijsService);
+
+                                if (txbAantalDagen != null)
+                                {
+                                    if (lbTotaalBedrag.Content == "0")
+                                    {
+                                        lbTotaalBedrag.Content = "1";
+                                    }
+                                    prijsServicedecimal *= Convert.ToInt32(txbAantalDagen.Text);
+                                    //de prijs maal het aantal geselcteerde dagen
+
+                                }
+
+                                //zorgt er voor dat de prijs in de listbox komt te staan
+                                
+                                totaalBetalen += prijsServicedecimal;
+                                lbTotaalBedrag.Content = totaalBetalen.ToString();
+                            }
+                        }
+                    }
+                    
+                }
+                else
+                {
+                    MessageBox.Show("vul een geldig aantal dagen in wat boven de 0 is");
+                }
+            }
+            catch 
+            {
+                MessageBox.Show("Vul cijfers in en geen letters");
+            }
+
+            
+        }
+
+        private void btnAfrekenen_Click(object sender, RoutedEventArgs e)
+        {
+            // de decimal zorgt er voor dat je een getal met een comma getal kan gebruiken
+            decimal betaling = Convert.ToDecimal(lbTotaalBedrag.Content);
+            if (betaling == 0)
+            {
+                MessageBox.Show("Je hebt nog niks besteld, bestel iets voordat je door gaat");
+            }
+            else
+            {
+                MessageBoxResult Antwoord = MessageBox.Show("Wil je afrekenen?", "Betaling", MessageBoxButton.YesNo);
+               
+                if (Antwoord == MessageBoxResult.Yes)
+                {
+                    //zorgt er voor dat alles wordt leeg gehaald en een bericht krijgt dat je hebt betaald
+                    MessageBox.Show("U hebt betaald.");
+                    txtVoornaam.Text = "";
+                    txtTussenvoegsel.Text = "";
+                    txtAchternaam.Text = "";
+                    txbAantalDagen.Text = "";
+                    lstVerzameling.Items.Clear();
+                    cmbFiets.SelectedIndex = -1;
+                    cmbVerzekering.SelectedIndex = -1;
+                    cmbService.SelectedIndex = -1;
+                    lbTotaalBedrag.Content = "0";
+                    totaalBetalen = 0;
+                }
+                else
+                {
+                    MessageBox.Show("Kom terug als je klaar bent");
+                }
+            }
+        }
+
+        private void btnVolgendeKlant_Click(object sender, RoutedEventArgs e)
+        {
+            //zorgt er voor dat alles wordt leeg gehaald
+            txtVoornaam.Text = "";
+            txtTussenvoegsel.Text = "";
+            txtAchternaam.Text = "";
+            txbAantalDagen.Text = "";
+            lstVerzameling.Items.Clear();
+            cmbFiets.SelectedIndex = -1;
+            cmbVerzekering.SelectedIndex = -1;
+            cmbService.SelectedIndex = -1;
+            lbTotaalBedrag.Content = "0";
+            totaalBetalen = 0;
+        }
+        private void cmbSoort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // de code hier onder zorgt er voor dat je alleen 1 combobox per keer geselecteerd mag worden
+            cmbVerzekering.SelectionChanged -= cmbVerzekering_SelectionChanged; //zet de eventhandeler uit
+            cmbService.SelectionChanged -= cmbService_SelectionChanged;
+            cmbVerzekering.SelectedIndex = -1;
+            cmbService.SelectedIndex = -1;
+            cmbVerzekering.SelectionChanged += cmbVerzekering_SelectionChanged;
+            cmbService.SelectionChanged += cmbService_SelectionChanged; //zet de eventhandeler weer aan
+
+        }
+
+        private void cmbVerzekering_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            cmbFiets.SelectionChanged -= cmbSoort_SelectionChanged;
+            cmbService.SelectionChanged -= cmbService_SelectionChanged;
+            cmbService.SelectedIndex = -1;
+            cmbFiets.SelectedIndex = -1;
+            cmbFiets.SelectionChanged += cmbSoort_SelectionChanged;
+            cmbService.SelectionChanged += cmbService_SelectionChanged;
+
+        }
+
+        private void cmbService_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            cmbFiets.SelectionChanged -= cmbSoort_SelectionChanged;
+            cmbVerzekering.SelectionChanged -= cmbVerzekering_SelectionChanged;
+            cmbFiets.SelectedIndex = -1;
+            cmbVerzekering.SelectedIndex = -1;
+            cmbFiets.SelectionChanged += cmbSoort_SelectionChanged;
+            cmbVerzekering.SelectionChanged += cmbVerzekering_SelectionChanged;
+
+        }
+
+        private void lstVerzameling_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                string[] split1 = lstVerzameling.SelectedItem.ToString().Split("€");
+                string[] split2 = split1[1].Split(" ");
+                decimal prijs = Convert.ToDecimal(split2[0]);
+                int dagen = Convert.ToInt32(split2[5]);
+                totaalBetalen -= prijs * dagen;
+                lbTotaalBedrag.Content = totaalBetalen.ToString();
+
+                lstVerzameling.Items.Remove(lstVerzameling.SelectedItem);
+                lstVerzameling.Items.Refresh();
+            }
+            catch
+            {
+                MessageBox.Show("lijkt er op dat er iets is fout gegaan");
+            }
+        }
+    }
+}
---
+++ Rekenmachine.xaml.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Automation.Provider;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Fietsenwinkel_allerlaatste_poging
+{
+    /// <summary>
+    /// decimaleraction logic for Rekenmachine.xaml
+    /// </summary>
+    public partial class Rekenmachine : Window
+    {
+        //de som die berekend kan worden
+        string Reken = "";
+        decimal Antwoord = 0;
+
+        public Rekenmachine()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNull_Click(object sender, RoutedEventArgs e)
+        {
+            txtBerekening.Text = Reken = Reken + 0;
+        }
+
+        private void btnEen_Click(object sender, RoutedEventArgs e)
+        {
+            txtBerekening.Text = Reken = Reken + 1;
+        }
+
+        private void btnTwee_Click(object sender, RoutedEventArgs e)
+        {
+            txtBerekening.Text = Reken = Reken + 2;
+        }
+
+        private void btnDrie_Click(object sender, RoutedEventArgs e)
+        {
+            txtBerekening.Text = Reken = Reken + 3;
+        }
+
+        private void btnVier_Click(object sender, RoutedEventArgs e)
+        {
+            txtBerekening.Text = Reken = Reken + 4;
+        }
+
+        private void btnVijf_Click(object sender, RoutedEventArgs e)
+        {
+            txtBerekening.Text = Reken = Reken + 5;
+        }
+
+        private void btnZes_Click(object sender, RoutedEventArgs e)
+        {
+            txtBerekening.Text = Reken = Reken + 6;
+            btnDellen.IsEnabled = true;
+            btnKeer.IsEnabled = true;
+        }
+
+        private void btnZeven_Click(object sender, RoutedEventArgs e)
+        {
+            txtBerekening.Text = Reken = Reken + 7;
+        }
+
+        private void btnAcht_Click(object sender, RoutedEventArgs e)
+        {
+            txtBerekening.Text = Reken = Reken + 8;
+        }
+
+        private void btnNegen_Click(object sender, RoutedEventArgs e)
+        {
+            txtBerekening.Text = Reken = Reken + 9;
+        }
+
+        private void btnDellen_Click(object sender, RoutedEventArgs e)
+        {
+            if (!txtBerekening.Text.EndsWith("+ ") && !txtBerekening.Text.EndsWith("- ") && !txtBerekening.Text.EndsWith("x ") && !txtBerekening.Text.EndsWith(": ") && !txtBerekening.Text.EndsWith(","))
+            {
+                txtBerekening.Text = Reken = Reken + " " + ":" + " ";
+                btnComma.IsEnabled = true;
+            }
+            else
+            {
+                MessageBox.Show("vul een tweede getal in");
+            }
+        }
+
+        private void btnKeer_Click(object sender, RoutedEventArgs e)
+        {
+            if (!txtBerekening.Text.EndsWith("+ ") && !txtBerekening.Text.EndsWith("- ") && !txtBerekening.Text.EndsWith("x ") && !txtBerekening.Text.EndsWith(": ") && !txtBerekening.Text.EndsWith(","))
+            {
+                txtBerekening.Text = Reken = Reken + " " + "x" + " ";
+                btnComma.IsEnabled = true;
+            }
+            else
+            {
+                MessageBox.Show("vul een tweede getal in");
+            }
+        }
+
+        private void btnPlus_Click(object sender, RoutedEventArgs e)
+        {
+            if (!txtBerekening.Text.EndsWith("+ ") && !txtBerekening.Text.EndsWith("- ") && !txtBerekening.Text.EndsWith("x ") && !txtBerekening.Text.EndsWith(": ") && !txtBerekening.Text.EndsWith(","))
+            {
+                txtBerekening.Text = Reken = Reken + " " + "+" + " ";
+                btnComma.IsEnabled = true;
+            }
+            else
+            {
+                MessageBox.Show("vul een tweede getal in");
+            }
+        }
+
+        private void btnMin_Click(object sender, RoutedEventArgs e)
+        {
+            if (!txtBerekening.Text.EndsWith("+ ") && !txtBerekening.Text.EndsWith("- ") && !txtBerekening.Text.EndsWith("x ") && !txtBerekening.Text.EndsWith(": ") && !txtBerekening.Text.EndsWith(","))
+            {
+                txtBerekening.Text = Reken = Reken + " " + "-" + " ";
+                btnComma.IsEnabled = true;
+            }
+            else
+            {
+                MessageBox.Show("vul een tweede getal in");
+            }
+        }
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            //zorgt er voor dat alles weg gaat en het and ook word gereset
+            Reken = "";
+            txtBerekening.Text = "";
+            txtAntwoord.Text = "";
+            btnComma.IsEnabled = true;
+
+        }
+
+        private void btnAntwoord_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (!txtBerekening.Text.EndsWith("+ ") && !txtBerekening.Text.EndsWith("- ") && !txtBerekening.Text.EndsWith("x ") && !txtBerekening.Text.EndsWith(": ") && !txtBerekening.Text.EndsWith(","))
+                {
+                    string[] mathDiv = Reken.Split(" ");
+                    if (mathDiv[1] == "+")
+                    {
+                        //pakt het eerste getal en doet plus het 2de getal
+                        decimal value1 = decimal.Parse(mathDiv[0]);
+                        decimal value2 = decimal.Parse(mathDiv[2]);
+                        Antwoord = value1 + value2;
+                        txtAntwoord.Text = Antwoord.ToString();
+                    }
+                    else if (mathDiv[1] == "-")
+                    {
+                        //pakt het eerste getal en doet min het 2de getal
+                        decimal value1 = decimal.Parse(mathDiv[0]);
+                        decimal value2 = decimal.Parse(mathDiv[2]);
+                        Antwoord = value1 - value2;
+                        txtAntwoord.Text = Antwoord.ToString();
+                    }
+                    else if (mathDiv[1] == ":")
+                    {
+                        //pakt het eerste getal en doet gedeeld door het 2de getal
+                        decimal value1 = decimal.Parse(mathDiv[0]);
+                        decimal value2 = decimal.Parse(mathDiv[2]);
+                        Antwoord = value1 / value2;
+                        txtAntwoord.Text = Antwoord.ToString();
+                    }
+                    else
+                    {
+                        //pakt het eerste getal en doet keer door het 2de getal
+                        decimal value1 = decimal.Parse(mathDiv[0]);
+                        decimal value2 = decimal.Parse(mathDiv[2]);
+                        Antwoord = value1 * value2;
+                        txtAntwoord.Text = Antwoord.ToString();
+                    }
+                    btnComma.IsEnabled = true;
+
+                }
+                else
+                {
+                    MessageBox.Show("vul Eerst een tweede getal in");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Je hebt nog niet de voledige berekening afgemaakt");
+            }
+        }
+
+        private void btnAns_Click(object sender, RoutedEventArgs e)
+        {
+            Reken = Antwoord.ToString();
+            txtBerekening.Text = Reken.ToString();
+            txtAntwoord.Text = "";
+            btnComma.IsEnabled = true;
+        }
+
+        private void btnComma_Click(object sender, RoutedEventArgs e)
+        {
+            if (!txtBerekening.Text.EndsWith("+ ") && !txtBerekening.Text.EndsWith("- ") && !txtBerekening.Text.EndsWith("x ") && !txtBerekening.Text.EndsWith(": ") && !txtBerekening.Text.EndsWith(",") && !txtBerekening.Text.EndsWith("0,"))
+            {
+
+                if (txtBerekening.Text == "")
+                {
+                    txtBerekening.Text = Reken = Reken + "0,";
+                }
+                else
+                {
+                    txtBerekening.Text = Reken = Reken + ",";
+                }
+                btnComma.IsEnabled = false;
+            }
+            else
+            {
+                txtBerekening.Text = Reken = Reken + "0,";
+                btnComma.IsEnabled = false;
+            }
+        }
+    }
+}
